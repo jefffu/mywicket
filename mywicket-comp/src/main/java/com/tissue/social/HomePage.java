0fcf35e9741636156fb057d32bcce0d66ee0d3c8
Jeff Fu <jefffu@tianji.com>
2014-06-24 16:04:12 +0800

    Test Login Form

--- mywicket-comp/src/main/java/com/tissue/social/HomePage.java
+++ mywicket-comp/src/main/java/com/tissue/social/HomePage.java
@@ -4,13 +4,50 @@ import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.PasswordTextField;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.Model;
 
 public class HomePage extends WebPage {
     private static final long serialVersionUID = 1l;
 
+    public static class LoginForm extends Form<String> {
+
+        private static final long serialVersionUID = 7747055281008833705L;
+
+        private TextField<String> usernameField;
+        private PasswordTextField passwordField;
+        private Label loginStatus;
+
+        public LoginForm() {
+            super("loginForm");
+
+            usernameField = new TextField<String>("username", Model.of(""));
+            passwordField = new PasswordTextField("password", Model.of(""));
+            loginStatus = new Label("loginStatus", Model.of(""));
+
+            add(usernameField);
+            add(passwordField);
+            add(loginStatus);
+        }
+
+        @Override
+        protected void onSubmit() {
+            String username = usernameField.getDefaultModelObjectAsString();
+            String password = passwordField.getDefaultModelObjectAsString();
+            if(username.equals("test") && password.equals("test")) {
+                loginStatus.setDefaultModelObject("Congratulations!");
+            } else {
+                loginStatus.setDefaultModelObject("Wrong username or password!");
+            }
+        }
+
+    }
+
     public HomePage() {
-        Form<String> form = new Form<String>("testForm");
+        Form<?> form = new LoginForm();
+        setVersioned(false);
         add(form);
 
         Page page = form.getPage();
